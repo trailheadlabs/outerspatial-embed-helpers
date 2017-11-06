02825512e489faeede53d6e41046add6d680c827
Nate Irwin <nate@irwin.xyz>
2017-11-06 15:57:32 -0700

    Switch to console.warn

--- src/iframe-parent-utilities.js
+++ src/iframe-parent-utilities.js
@@ -1,66 +1,66 @@
 if (typeof window.OuterSpatialEmbedHelpers !== 'object') {
-  console.error('window.OuterSpatialEmbedHelpers must be an object');
+  console.warning('The window.OuterSpatialEmbedHelpers object was not found');
 } else if (typeof window.OuterSpatialEmbedHelpers.iframeElement !== 'object') {
-  console.error('window.OuterSpatialEmbedHelpers.iframeElement must be a DOM element object');
-}
+  console.warning('The window.OuterSpatialEmbedHelpers.iframeElement property must be a DOM element object');
+} else {
+  (function () {
+    var bodyMargin;
+    var bodyOverflow;
+    var bodyPadding;
+    var containerLeft;
+    var containerPosition;
+    var containerTop;
+    var height;
+    var iframe = window.OuterSpatialEmbedHelpers.iframeElement;
+    var width;
 
-(function () {
-  var bodyMargin;
-  var bodyOverflow;
-  var bodyPadding;
-  var containerLeft;
-  var containerPosition;
-  var containerTop;
-  var height;
-  var iframe = window.OuterSpatialEmbedHelpers.iframeElement;
-  var width;
-
-  function enterFullscreen () {
-    bodyMargin = document.body.style.margin;
-    bodyOverflow = document.body.style.overflow;
-    bodyPadding = document.body.style.padding;
-    document.body.style.margin = '0';
-    document.body.style.overflow = 'hidden';
-    document.body.style.padding = '0';
-    containerLeft = iframe.style.left;
-    containerPosition = iframe.style.position;
-    containerTop = iframe.style.top;
-    height = iframe.style.height;
-    width = iframe.style.width;
-    iframe.style.height = '100%';
-    iframe.style.left = '0';
-    iframe.style.position = 'fixed';
-    iframe.style.top = '0';
-    iframe.style.width = '100%';
-  }
-  function exitFullscreen () {
-    document.body.style.margin = bodyMargin;
-    document.body.style.overflow = bodyOverflow;
-    document.body.style.padding = bodyPadding;
-    iframe.style.height = height;
-    iframe.style.left = containerLeft;
-    iframe.style.position = containerPosition;
-    iframe.style.top = containerTop;
-    iframe.style.width = width;
-  }
-
-  if (window.location.hash) {
-    iframe.src += window.location.hash;
-  }
-
-  window.addEventListener('message', function (event) {
-    var data = event.data;
-
-    switch (data.id) {
-      case 'outerspatial_map_library-enterfullscreen':
-        enterFullscreen();
-        break;
-      case 'outerspatial_map_library-exitfullscreen':
-        exitFullscreen();
-        break;
-      case 'outerspatial_map_library-hashchange':
-        window.history.replaceState(undefined, undefined, data.hash);
-        break;
+    function enterFullscreen () {
+      bodyMargin = document.body.style.margin;
+      bodyOverflow = document.body.style.overflow;
+      bodyPadding = document.body.style.padding;
+      document.body.style.margin = '0';
+      document.body.style.overflow = 'hidden';
+      document.body.style.padding = '0';
+      containerLeft = iframe.style.left;
+      containerPosition = iframe.style.position;
+      containerTop = iframe.style.top;
+      height = iframe.style.height;
+      width = iframe.style.width;
+      iframe.style.height = '100%';
+      iframe.style.left = '0';
+      iframe.style.position = 'fixed';
+      iframe.style.top = '0';
+      iframe.style.width = '100%';
     }
-  }, false);
-})();
+    function exitFullscreen () {
+      document.body.style.margin = bodyMargin;
+      document.body.style.overflow = bodyOverflow;
+      document.body.style.padding = bodyPadding;
+      iframe.style.height = height;
+      iframe.style.left = containerLeft;
+      iframe.style.position = containerPosition;
+      iframe.style.top = containerTop;
+      iframe.style.width = width;
+    }
+
+    if (window.location.hash) {
+      iframe.src += window.location.hash;
+    }
+
+    window.addEventListener('message', function (event) {
+      var data = event.data;
+
+      switch (data.id) {
+        case 'outerspatial_map_library-enterfullscreen':
+          enterFullscreen();
+          break;
+        case 'outerspatial_map_library-exitfullscreen':
+          exitFullscreen();
+          break;
+        case 'outerspatial_map_library-hashchange':
+          window.history.replaceState(undefined, undefined, data.hash);
+          break;
+      }
+    }, false);
+  })();
+}
--- src/load-builder-map.js
+++ src/load-builder-map.js
@@ -1,43 +1,43 @@
 if (typeof window.OuterSpatialEmbedHelpers !== 'object') {
-  console.error('window.OuterSpatialEmbedHelpers must be an object');
+  console.warning('window.OuterSpatialEmbedHelpers must be an object');
 } else if (isNaN(window.OuterSpatialEmbedHelpers.builderMapId)) {
-  console.error('window.OuterSpatialEmbedHelpers.builderMapId must be a number');
+  console.warning('window.OuterSpatialEmbedHelpers.builderMapId must be a number');
 } else if (typeof window.OuterSpatialEmbedHelpers.element !== 'object') {
-  console.error('window.OuterSpatialEmbedHelpers.element must be a DOM element object');
-}
+  console.warning('window.OuterSpatialEmbedHelpers.element must be a DOM element object');
+} else {
+  (function () {
+    var builderMapId = window.OuterSpatialEmbedHelpers.builderMapId;
+    var element = window.OuterSpatialEmbedHelpers.element;
+    var request = new window.XMLHttpRequest();
 
-(function () {
-  var builderMapId = window.OuterSpatialEmbedHelpers.builderMapId;
-  var element = window.OuterSpatialEmbedHelpers.element;
-  var request = new window.XMLHttpRequest();
+    request.open('GET', 'https://www.outerspatial.com/builder_maps/' + builderMapId + '.json', true);
+    request.onerror = function () {
+      console.error('The Builder map could not be retrieved from OuterSpatial');
+    };
+    request.onload = function () {
+      if (request.status >= 200 && request.status < 400) {
+        var response = JSON.parse(request.responseText);
 
-  request.open('GET', 'https://www.outerspatial.com/builder_maps/' + builderMapId + '.json', true);
-  request.onerror = function () {
-    console.error('The Builder map could not be retrieved from OuterSpatial');
-  };
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 400) {
-      var response = JSON.parse(request.responseText);
+        if (typeof response === 'object') {
+          var config = response.config;
 
-      if (typeof response === 'object') {
-        var config = response.config;
+          if (typeof config === 'object') {
+            var script = document.createElement('script');
 
-        if (typeof config === 'object') {
-          var script = document.createElement('script');
-
-          config.div = element;
-          window.OuterSpatial = config;
-          script.src = 'https://cdn.outerspatial.com/libs/outerspatial.js/' + config.meta.outerspatialJsVersion + '/outerspatial-bootstrap.min.js';
-          document.body.append(script);
+            config.div = element;
+            window.OuterSpatial = config;
+            script.src = 'https://cdn.outerspatial.com/libs/outerspatial.js/' + config.meta.outerspatialJsVersion + '/outerspatial-bootstrap.min.js';
+            document.body.append(script);
+          } else {
+            console.error('There was a problem parsing the Builder map JSON returned from OuterSpatial');
+          }
         } else {
           console.error('There was a problem parsing the Builder map JSON returned from OuterSpatial');
         }
       } else {
-        console.error('There was a problem parsing the Builder map JSON returned from OuterSpatial');
+        console.error('The Builder map could not be retrieved from OuterSpatial');
       }
-    } else {
-      console.error('The Builder map could not be retrieved from OuterSpatial');
-    }
-  };
-  request.send();
-})();
+    };
+    request.send();
+  })();
+}
